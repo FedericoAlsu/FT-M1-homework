e704ff15d0bb2b64c305138c8e0f821e158c2f8f
mitoken <[200~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~>
2023-01-05 21:28:03 -0300

    homework 4

--- 03-JavaScriptAvanzado-II/homework/homework.js
+++ 03-JavaScriptAvanzado-II/homework/homework.js
@@ -13,7 +13,14 @@ nuevoContador()     // 2
 const otroContador = counter()
 otroContador()      // 1
 otroContador()      // 2 */
-function counter() {}
+function counter() {
+  let a = 0;
+  function contar(){
+    a++;
+    return a;
+  }
+  return contar;
+}
 
 /* Ejercicio 2
 Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback 
@@ -33,24 +40,48 @@ otra vez cálculos que ya se hicieron anteriormente.
   squareCache(5)    // invocará a square(5), almacenará el resultado y lo retornará
   squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) */
 
-function cacheFunction(cb) {}
+
+function cacheFunction(cb) {
+  let obj = {}; // {5: 10, 2: 4},
+    return function(arg){
+      if(obj.hasOwnProperty(arg)) return obj[arg]
+  
+      obj[arg]= cb(arg)
+      return obj[arg]
+    }
+  }
+/*
+let result = {};
+  if(result.hasOwnProperty("square")){
+    return result.square;
+  }
+  else if(!result.hasOwnProperty("square")){
+    function square_cache(){
+      result.square = cb;
+    }
+    square_cache();
+    return result.square;
+  }
+*/
+
 
 //----------------------------------------
 
 // Bind
 
 var instructor = {
-   nombre: 'Franco',
-   edad: 25,
+  nombre: 'Franco',
+  edad: 25,
 };
 
 var alumno = {
-   nombre: 'Juan',
-   curso: 'FullStack',
+  nombre: 'Juan',
+  curso: 'FullStack',
 };
 
 function getNombre() {
-  return this.nombre;}
+ return this.nombre;
+}
 
 /*
   Ejercicio 3
@@ -58,21 +89,23 @@ function getNombre() {
   Usando el método bind() guardar, en las dos variables declaradas a continuación, dos funciones que actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
 */
 
-let getNombreInstructor = getNombre.bind();
-let getNombreAlumno = getNombre.bind();
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
+
 
 /*
   Ejercicio 4
   Sin modificar la función crearCadena, usar bind para guardar, en las tres variables declaradas a continuación, tres funciones que retornen una cadena (string) y el delimitador especificado (asteriscos, guiones, y guiones bajos, respectivamente). Las funciones obtenidas deberían recibir solamente un argumento - la cadena de texto - ya que los otros argumentos habrán sido "bindeados". 
 */
 
+
 function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
-    return delimitadorIzquierda + cadena + delimitadorDerecha;
+  return delimitadorIzquierda + cadena + delimitadorDerecha;
 }
 
-let textoAsteriscos = crearCadena.bind();
-let textoGuiones = crearCadena.bind();
-let textoUnderscore = crearCadena.bind();
+let textoAsteriscos = crearCadena.bind(this, "*", "*");
+let textoGuiones = crearCadena.bind(this, "-", "-");
+let textoUnderscore = crearCadena.bind(null, "_", "_");
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -1,13 +1,66 @@
 'use strict';
 
 // EJERCICIO 1
-function nFactorial(n) {}
+function nFactorial(n) {
+   if(n == 0 || n === 1) return 1
+   else if( n < 0) return 0
+   else return n * nFactorial(n - 1)
+ }
+
+
+
 
 // EJERCICIO 2
-function nFibonacci(n) {}
+function nFibonacci(n) {
+      if (n < 2) return n;
+      else{
+         return nFibonacci(n-1) + nFibonacci(n-2);
+      }
+}
+
+
 
 // EJERCICIO 3
-function Queue() {}
+function Queue() {
+   class Queue {
+     
+      constructor(){
+         this.obj = {};
+      }
+      enqueue(nuevo){
+         this.obj[nuevo] = nuevo;
+         return this.obj[nuevo];
+      }
+      dequeue(quitar = null){
+         if(quitar == null){
+            let first = Object.keys(this.obj)[0];
+            delete this.obj[first];
+
+            if(parseInt(first)){
+               return parseInt(first)
+            }else{
+               return first;
+            }
+            
+
+         }
+         else if(this.obj.hasOwnProperty(quitar)){
+            delete this.obj[quitar];
+            return quitar;
+         }
+         else{
+            return undefined;
+         }
+      }
+      size(){
+
+         return Object.keys(this.obj).length;
+      }
+   }
+   return new Queue;
+}
+
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
